c041afc52683060eacfc9d41f1f6588b6dd01b6e
Sajani Jayasinghe <sajani@codevus.com>
2022-05-09 19:27:32 +0530

    frontend packages

---
+++ Frontend/src/components/customerLogin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class customerLogin extends Component {
+ 
+ 
+  render() {
+    return (
+    <h1>Hello</h1>
+    )
+  }
+}
