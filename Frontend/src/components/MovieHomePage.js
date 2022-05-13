1e8b24455edbbbe1f0e1f302424d0182c3caad54
Sajani Jayasinghe <sajani@codevus.com>
2022-05-13 19:35:46 +0530

    main homepage design changes

--- Frontend/src/components/MovieHomePage.js
+++ Frontend/src/components/MovieHomePage.js
@@ -1,98 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react'
 import MainHomePageNavBar from './MainHomePageLayOut/MainHomePageNavBar';
 import Footer from './footer';
-import {FaArrowCircleLeft, FaArrowCircleRight} from 'react-icons/fa'
-import CircularProgress from '@material-ui/core/CircularProgress';
+
+export default class MovieHomePage extends Component {
+  render() {
+    return (
+      <div style={{ backgroundColor:"#2B3856" }} >
+          <MainHomePageNavBar/>
+          <div style={{ backgroundColor:"#0C090A" ,height:"800px"}} >
+
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+
+    <div class="carousel-item">
+      <img src="https://www.reelrecon.com/wp-content/uploads/2014/03/enders_game_2013_movie-wide.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+  
+    <div class="carousel-item">
+      <img src="https://nettv4u.com/imagine/-raja-cheyyi-vesthe--movie-first-look-poster-exclusive-3.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+  
+    <div class="carousel-item">
+      <img src="https://webneel.com/daily/sites/default/files/images/daily/01-2019/9-movie-poster-design-kollywood-tamil-balloon-prathoolnt.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+
+   <div class="carousel-item active">
+      <img src="https://cdn.wallpapersafari.com/42/50/8mKbCk.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+
+    <div class="carousel-item">
+      <img src="http://images6.fanpop.com/image/photos/37000000/You-re-All-Surrounded-korean-dramas-37017261-1920-1080.jpg" class="d-block w-100" alt="..."></img>
+    </div>
 
 
-function MovieHomePage() {
-    const[Sources, setSources] = useState([])
-    const [isLoading, setLoading] = useState(true)
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
-    const shiftLeft = () =>{
-        const newOrder = []
 
-        Sources.forEach((src, index) =>{
-            const newIndex = --index < 0 ? Sources.length -1 : index
-            newOrder[newIndex] = src
-          })
-      
-          setSources(newOrder)
-      
-        }
-        const shiftRight = () =>{
-        const newOrder = []
           
-            Sources.forEach((src, index) =>{
-              const newIndex = ++index >= Sources.length ? 0 : index
-              newOrder[newIndex] = src
-                
-            })
-        
-            setSources(newOrder)
-        }
-        useEffect(() => {
-            setLoading(true)
-            const getMovies = async () => {
-               try {
-                await axios.get(`http://localhost:8070/api/movie/display`)
-                .then((res) => {
-                  setSources(res.data.existingMovie)
-                  setLoading(false)
-                })
-                .catch((err) => {
-                  console.log(err)
-                })
-             } catch (error) {
-                console.log(error.message)
-             }
-          }
-          
-          getMovies()
-       }, [])
- 
-       if (isLoading) {
-        return <div className="d-flex justify-content-center" style={{ paddingTop: 400 }}>
-           <CircularProgress />
-        </div>
-      }
-      return (
-        <div>
-           <MainHomePageNavBar/>
-       
-        <div className="">
-      
-        <div className="Carousel" >
-          
-          
-          <div className="container">
-          <div>
-          <FaArrowCircleLeft class="left-arrow" onClick={shiftLeft} size="32"/>
-          </div>
-          
-          
-          {Sources.map((src, index) =>
-              <div  key={index}>
-                <img id={'pic' + index} src={src.banner}
-                alt="img" style={{width:'450px'}} />
-                
-              </div>
-              
-          )}
-          
-          <div>&nbsp;
-            <FaArrowCircleRight class="right-arrow" onClick={shiftRight} size="32"/>
           </div>
 
-            
-       </div>
-       </div>
-       </div>
-       <Footer/>
-       </div>  
-      
-  )
+     
+          <Footer/>
+      </div>
+    )
+  }
 }
-
-export default MovieHomePage;
